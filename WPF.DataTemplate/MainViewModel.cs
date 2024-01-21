f69ed477d6030d0a902a125cc900afa8c5a88b73
yunasim <ynsim@solidit.co.kr>
2024-01-22 00:10:46 +0900

    #✨feature: 소스 초기 업로드
    - 데이터 모델 구현
    - 뷰 모델 구현
    - 뷰에는 리스트만 추가하고, 아이템 소스에 뷰 모델만 연결

---
+++ WPF.DataTemplate/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WpfBase;
+
+namespace WPF.DataTemplate
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private List<Student> students = default;
+
+        public MainViewModel()
+        {
+            students = Student.Students;
+        }
+
+        public  List<Student> Students
+        {
+            get => students;
+            set
+            {
+                students = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+}
---
+++ WPF.DataTemplate/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.DataTemplate
+{
+    public class Student
+    {
+        public string Name { get; set; } = "";
+        public string Gender { get; set; } = "";
+
+        public string BirthDay { get; set; } = "";
+        public string Address { get; set; } = "";
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Gender: {Gender}, BirthDay: {BirthDay}, Address: {Address}";
+        }
+
+        public static List<Student> Students => new()
+        {
+            new Student {Name = "심유나", Gender = "여자", BirthDay = "1992-11-19", Address  = ""},
+            new Student {Name = "이가영", Gender = "여자", BirthDay = "1992-11-05", Address  = ""}
+        };
+    }
+}
