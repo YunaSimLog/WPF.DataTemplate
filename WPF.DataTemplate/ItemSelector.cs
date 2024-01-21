8d83fe792df9584c042cb77528ceb312512aa0fa
yunasim <ynsim@solidit.co.kr>
2024-01-22 00:26:04 +0900

    #😎update: ItemSelector를 추가하여, 아이템 유형에 따라 특정 아이템 템플릿 리소스 사용하게 처리

---
+++ WPF.DataTemplate/ItemSelector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPF.DataTemplate
+{
+    public class ItemSelector : DataTemplateSelector
+    {
+        public override System.Windows.DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            Student? student = item as Student;
+            FrameworkElement frameworkElement = container as FrameworkElement;
+
+            return (System.Windows.DataTemplate)frameworkElement.FindResource(
+                student?.Gender == "여자"
+                    ? "Woman"
+                    : "Man");
+        }
+    }
+}
--- WPF.DataTemplate/Student.cs
+++ WPF.DataTemplate/Student.cs
@@ -22,8 +22,9 @@ namespace WPF.DataTemplate
 
         public static List<Student> Students => new()
         {
-            new Student {Name = "심유나", Gender = "여자", BirthDay = "1992-11-19", Address  = ""},
-            new Student {Name = "이가영", Gender = "여자", BirthDay = "1992-11-05", Address  = ""}
+            new Student {Name = "심유나", Gender = "여자", BirthDay = "1992-11-19", Address  = "수원"},
+            new Student {Name = "이가영", Gender = "여자", BirthDay = "1992-11-05", Address  = "수원"},
+            new Student {Name = "김철수", Gender = "남자", BirthDay = "1997-01-01", Address  = "인천"},
         };
     }
 }
